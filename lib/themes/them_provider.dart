78ca01ae17873beb4060c5a904c7bb53570d0366
Rinshadk786 <rinur924@gmail.com>
2025-07-23 22:36:51 +0530

    shared pref

--- lib/themes/them_provider.dart
+++ lib/themes/them_provider.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dark_mode.dart';
 import 'light_mode.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  late ThemeData _themeData = lightMode;
+  late ThemeData _themeData;
+  final String _key = "isDarkMode";
+
+  ThemeProvider() {
+    _loadTheme();
+  }
 
   ThemeData get themeData => _themeData;
   bool get isDarkMode => _themeData == darkMode;
 
-  set themeData(ThemeData themeData) {
-    _themeData = themeData;
+  void toggleTheme() async {
+    _themeData = isDarkMode ? lightMode : darkMode;
+    await _saveTheme(isDarkMode); // Save the new value
     notifyListeners();
   }
 
-  void toggleTheme() {
-    _themeData = _themeData == lightMode ? darkMode : lightMode;
+  Future<void> _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isDark = prefs.getBool(_key) ?? false; // default to light
+    _themeData = isDark ? darkMode : lightMode;
     notifyListeners();
+  }
+
+  Future<void> _saveTheme(bool isDark) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_key, isDark);
   }
 }
